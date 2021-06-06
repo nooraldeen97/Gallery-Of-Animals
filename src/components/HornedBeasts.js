6311f5a66d7db7a8e1b4bd251dbeb508a8a6e812
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-06-06 16:30:58 +0300

    finished lab-01

---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class HornedBeasts extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>{this.props.title}</h2>
+                <img src={this.props.imageUrl} alt={this.props.title}/>
+                <p>{this.props.description}</p>
+            </div>
+        )
+    }
+}
+
+export default HornedBeasts;

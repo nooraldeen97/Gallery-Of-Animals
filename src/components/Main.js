a8f1b5cff1826d4fc3d8cd99f0f89241b9787a2a
nooraldeen97 <noor-aldeen97@hotmail.com>
2021-06-09 23:46:07 +0300

    finished lab-04

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import CardColumns from 'react-bootstrap/CardColumns';
-
+import FormInfo from './FormInfo';
+import data from './data.json';
 
 class Main extends React.Component {
 
+constructor(props){
+    super(props)
+    this.state={
+        rederedHorns:data
+    }
+}
+
    
-    
+    renderedCard=(num)=>{
+        
+        
+    let result;
+     if (num) {
+       result = data.filter(item =>{
+        if(item.horns == num)
+       return item;
+       })
+        
+      //  return result;
+      } else {
+        
+        result = data;
+      }
+
+        this.filteredArr(result)
+    }
+
+    filteredArr=(result)=>{
+        this.setState({
+            rederedHorns: result
+        })
+    }
+
+
     render(){
 
     return(
+        <>
+        <FormInfo
+        renderedCard={this.renderedCard}
+        />
 
    <CardColumns>
-        {this.props.myData.map((animal)=>{
+        {this.state.rederedHorns.map((animal)=>{
             return(
             <HornedBeasts
            title={animal.title}
@@ -23,6 +60,7 @@ class Main extends React.Component {
             )
         })}
     </CardColumns>
+    </>
             
         )
     }
